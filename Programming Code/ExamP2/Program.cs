ad65ab02780ef3e630b320c857817bae370061b3
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-11 16:06:16 +0400

    Add files via upload

---
+++ Programming Code/ExamP2/Program.cs
@@ -0,0 +1,64 @@
+﻿using ExamP2;
+
+namespace ExamP2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //In the Main method, create objects and demonstrate polymorphism.
+
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            Console.Write("Dog sounds: "); myDog.MakeSound();
+
+
+            Console.Write("Cat sounds: "); myCat.MakeSound();
+
+
+
+
+        }
+    }
+
+    //Create a base class Animal with method MakeSound(). 
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some animal sound");
+        }
+
+        
+    }
+
+    //Create two derived classes: Dog and Cat, each overriding MakeSound() with a different message.
+
+    public class Dog : Animal {
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Ho,Ho");
+        }
+
+
+    }
+
+
+    public class Cat : Animal {
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+
+}
+
+
+
+
+ 
