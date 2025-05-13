667fbab5f0056e4279ab5e6e2445fcd2210511d4
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-13 11:20:17 +0400

    Add files via upload

---
+++ Programming Code/C# Programming/CodeAcademyShopSolution/CodeAcademyShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeAcademyShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
