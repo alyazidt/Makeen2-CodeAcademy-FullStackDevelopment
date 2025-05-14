a5c57b3bf03cb74107f30e0a93ad363c4f23e733
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-14 09:53:59 +0400

    new update12

---
+++ Programming Code/C# Programming/MirrorInt/MirrorInt/Helpers/Globe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirrorInt.Helpers
+{
+    public static class Globe
+    {
+
+        public static string Mirror(int x)
+        {
+            var str = x.ToString().ToArray();
+            Array.Reverse(str);
+            var result = new string(str);
+            return result;
+
+
+        }
+
+    }
+}
---
+++ Programming Code/C# Programming/MirrorInt/MirrorInt/Program.cs
@@ -0,0 +1,14 @@
+﻿using MirrorInt.Helpers;
+
+namespace MirrorInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 12345;
+            string res = Globe.Mirror(x);
+            Console.WriteLine(res);
+        }
+    }
+}
