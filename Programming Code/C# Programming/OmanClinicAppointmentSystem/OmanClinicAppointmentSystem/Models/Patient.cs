7be193f3bae65145eb1152a72fdc246bac6ebf68
Al-Yazid Al-Mukhaini <alyazid88@gmail.com>
2025-05-17 18:42:03 +0400

    update assignmentV1

---
+++ Programming Code/C# Programming/OmanClinicAppointmentSystem/OmanClinicAppointmentSystem/Models/Patient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmanClinicAppointmentSystem.Models
+{
+    internal class Patient
+    {
+        public string Name { get; set; }
+        
+        public string NationalID { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+        public Patient (string name, string nationalID, string phoneNumber)
+        {
+            Name = name;
+            NationalID = nationalID;
+            PhoneNumber = phoneNumber;
+        }
+
+
+        public void RegisterNewPatient()
+        {
+            Console.WriteLine($"Name: {Name}, National ID: {NationalID}, Phone Number: {PhoneNumber}");
+        }
+
+
+
+
+
+    }
+}
